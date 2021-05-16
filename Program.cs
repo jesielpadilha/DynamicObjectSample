e4dd6303d740799adddeb8e4a2d052a4190e5f26
jesiel-unica <jpadilha@tecnologia-unica.com.br>
2021-05-16 19:01:47 -0300

    Inclusion of dynamic object example using Dictionary.

--- Program.cs
+++ Program.cs
@@ -14,9 +14,25 @@ namespace DynamicObjectSample
             obj.FirstName = "Ellen";
             obj.LastName = "Adams";
             obj.Age = 20;
-            obj.Todos = new List<string> {"task 1", "task 2", "task 3"};
+            obj.Todos = new List<string> { "task 1", "task 2", "task 3" };
+            obj.Addresses = new List<object> {
+                new {City = "New York", State = "NY", Country = "US"},
+                new {City = "London", State = "", Country = "UK"}
+            };
 
-            var json = JsonConvert.SerializeObject(obj);
+
+            var objDicionary = new Dictionary<string, object>();
+            objDicionary.Add("FirstName", "Ellen");
+            objDicionary.Add("LastName", "Adams");
+            objDicionary.Add("Age", 20);
+            objDicionary.Add("Nickname", "El");
+            objDicionary.Add("Todos", new List<string> { "task 1", "task 2", "task 3" });
+            objDicionary.Add("Addresses", new List<object> {
+                new {City = "New York", State = "NY", Country = "US"},
+                new {City = "London", State = "", Country = "UK"}
+            });
+
+            var json = JsonConvert.SerializeObject(objDicionary, Formatting.Indented);
             Console.WriteLine($"\n**** Json Object: \n{json}\n");
         }
     }
